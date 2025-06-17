4c133d5e0b5c6396a2394588bad20471e0ce1ce0
devops979 <devops979@gmail.com>
2025-06-17 14:37:53 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web-servers" {
   count                  = length(var.public_subnets)
   ami                    = var.ami_name
   instance_type          = var.instance_type
-  subnet_id              = var.public_subnets
+  subnet_id = var.public_subnets[count.index]
   vpc_name                 = var.vpc_name
   associate_public_ip_address= true
   vpc_security_group_ids = [var.sg_id]
